bbc39a5fc1036cb770798811743987e56b5fc1ab
jankrynauw <jan@alisx.com>
2023-06-03 13:21:47 +0200

    chore: minor tweak to docs.

--- docs.go
+++ docs.go
@@ -1,23 +1,4 @@
 /*
-	Package main is a collection of lightweight packages used by builders on the Alis Build platform.
-
-The Alis Build platform enables individuals and teams to build their own digital innovations.
-
-The Alis Build Platform
-Developing high-quality software requires meeting several demanding requirements such as scalability, accessibility,
-security, usability, and interoperability with other digital services. However, these requirements entail significant
-costs and expertise, particularly at a larger scale, which can create daunting barriers to entry for both individuals
-and organizations.
-
-While there are existing technologies and standards that could reduce these barriers, they are currently disjointed
-and require considerable effort to consolidate and effectively leverage before their benefits can be realized.
-To address this issue, we have carefully analyzed these complex problems and identified the best open-source
-technologies and standards. By integrating these resources, we aim to make it easier for users to build innovative
-solutions by following three simple steps: define, build, and deploy.
-
-Our aim is to make it accessible for you to build your own innovation by following the three easy steps: define,
-build and deploy.
-
-To get started on the platform visit: https://alis.build
+Package main is a collection of lightweight packages used by builders on the Alis Build platform.
 */
 package main //import "go.alis.build"
