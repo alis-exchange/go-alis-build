1444019d576edf59c7452d1bbe4980993b59be65
Jan Krynauw <jan@alisx.com>
2024-02-22 15:27:53 +0200

    chore(client): upgrade packages

--- client/validation.go
+++ client/validation.go
@@ -1,9 +1,10 @@
 package client
 
 import (
+	"regexp"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"regexp"
 )
 
 // validateArgument validates an argument and returns an error if not valid.
