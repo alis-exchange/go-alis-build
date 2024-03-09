196b6bb9aaaa2577c7357c9d58b70de6326ca396
Jan Krynauw <jan@alisx.com>
2024-03-09 12:14:02 +0200

    feat(validation): initial commit.

---
+++ validation/docs.go
@@ -0,0 +1,17 @@
+// Copyright 2023 The Alis Build Platform. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package validations will ensure data integrity by enforcing rules about data
+formats, required fields, and other constraints. It aims to improve API
+security, provide better user experience through informative error messages,
+and enhance overall code maintainability by offering a structured approach
+to validation logic.
+
+# Purpose
+  - **Centralized Validation Logic**: The package provides a structured and reusable way to validate incoming HTTP requests within your Go-based API projects.
+  - **Data Integrity**: The core function of the package is to ensure that the data within API requests aligns with predefined rules and structures.
+  - **Error Handling**: The package likely handles invalid requests gracefully, providing clear error messages that can be returned to the clie
+*/
+package validation // import "go.alis.build/validation"
---
+++ validation/go.mod
@@ -0,0 +1,3 @@
+module go.alis.build/validation
+
+go 1.22
