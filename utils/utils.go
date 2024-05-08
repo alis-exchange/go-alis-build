b9282c3b895d59e11bcaf45049491d5d3473041b
newtonnthiga <newton@alisx.com>
2024-05-08 16:27:27 +0300

    feat(utils): add Transform function

--- utils/utils.go
+++ utils/utils.go
@@ -10,3 +10,17 @@ func Contains[T comparable](s []T, searchTerm T) bool {
 	}
 	return false
 }
+
+// Transform is a utility function for transforming the elements of a slice.
+//
+// For example, you can use it to double the values of a slice of integers:
+//
+//	ints := []int{1, 2, 3}
+//	doubled := Transform(ints, func(i int) int { return i * 2 })
+func Transform[T, U any](arr []T, fn func(T) U) []U {
+	result := make([]U, len(arr))
+	for i, v := range arr {
+		result[i] = fn(v)
+	}
+	return result
+}
