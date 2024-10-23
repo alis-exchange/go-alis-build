c7f2c5d82d38c28cb03b39ba43c96579ea6c78ab
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2024-10-23 14:25:11 +0000

    feat(validator): sub domain rule

--- validator/a_string.go
+++ validator/a_string.go
@@ -429,6 +429,29 @@ func (s *str) IsValidTopLevelDomain() *Rule {
 	return newPrimitiveRule(id, descr, args, isViolatedFunc)
 }
 
+func (s *str) IsValidSubDomain() *Rule {
+	id := fmt.Sprintf("subdomain(%s)", s.description)
+	descr := &Descriptions{
+		rule:         fmt.Sprintf("%s must be a valid sub domain", s.getDescription()),
+		notRule:      fmt.Sprintf("%s must not be a valid sub domain", s.getDescription()),
+		condition:    fmt.Sprintf("%s is a valid sub domain", s.getDescription()),
+		notCondition: fmt.Sprintf("%s is not a valid sub domain", s.getDescription()),
+	}
+	args := []argI{s}
+	// can be console.alis.build but cannot be alis.build
+	// in essence, must have a subdomain
+	isViolatedFunc := func(msg protoreflect.ProtoMessage) (bool, error) {
+		for _, val := range s.getValues(s.v, msg) {
+			subDomainRegex := `^([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.){2,}[a-zA-Z]{2,}$`
+			if !regexp.MustCompile(subDomainRegex).MatchString(val) {
+				return true, nil
+			}
+		}
+		return false, nil
+	}
+	return newPrimitiveRule(id, descr, args, isViolatedFunc)
+}
+
 func (s *str) IsValidEmail() *Rule {
 	id := fmt.Sprintf("email(%s)", s.description)
 	descr := &Descriptions{
