1fdade80ab45a4f03459ab0d49fc04d78009d4cf
jan@alisx.com <jan@alisx.com>
2025-03-24 07:29:32 +0200

    docs(validation): add example usage for NewValidator

--- validation/validation.go
+++ validation/validation.go
@@ -41,7 +41,32 @@ type Condition interface {
 	wrap()
 }
 
-// Creates a new Validator instance.
+/*
+Creates a new Validator instance.
+
+Example:
+
+	v := validation.NewValidator()
+	v.String("name", req.GetName()).IsPopulated()
+	v.Int32("age", req.GetAge()).Gt(18)
+	v.If(v.Enum("status", req.GetStatus()).Is(validation.User_ACTIVE)).Then(
+		v.Timestamp("update_time", req.GetUpdateTime()).NotInFuture(),
+	)
+	v.Custom("name must be populated", req.GetName() != "")
+	v.Custom("age must be greater than 18", req.GetAge() > 18)
+	v.CustomEvaluated("if status is ACTIVE, update time must not be in the future", func() bool {
+		if req.GetStatus() == validation.User_ACTIVE {
+			return req.GetUpdateTime().AsTime().Before(time.Now())
+		}
+		return true
+	})
+
+	// validate
+	err := v.Validate()
+	if err != nil {
+		return status.Errorf(codes.InvalidArgument, "%s", err.Error())
+	}
+*/
 func NewValidator() *Validator {
 	return &Validator{}
 }
